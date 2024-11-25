c1fba9d28a15f8a9501c5fddc417b14bcc98da6d
numq <numq.dev@gmail.com>
2024-11-26 01:56:13 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "com.github.numq"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
