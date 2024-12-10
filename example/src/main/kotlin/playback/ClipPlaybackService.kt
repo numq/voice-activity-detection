f0691c2751bad8976fea71ad41893cadadf60173
numq <numq.dev@gmail.com>
2024-12-11 02:46:05 +0300

    Provide refactoring

---
+++ example/src/main/kotlin/playback/ClipPlaybackService.kt
@@ -0,0 +1,62 @@
+package playback
+
+import device.Device
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.io.ByteArrayOutputStream
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.LineEvent
+
+class ClipPlaybackService : PlaybackService {
+    private val mutex = Mutex()
+
+    private var baos = ByteArrayOutputStream()
+
+    override suspend fun write(pcmBytes: ByteArray) = mutex.withLock {
+        runCatching {
+            baos.write(pcmBytes)
+        }
+    }
+
+    override suspend fun play(device: Device) = mutex.withLock {
+        runCatching {
+            if (baos.size() > 0) {
+                val pcmBytes = baos.toByteArray()
+
+                baos.reset()
+
+                val audioFormat = with(device) {
+                    AudioFormat(
+                        sampleRate.toFloat(),
+                        sampleSizeInBits,
+                        channels,
+                        isSigned,
+                        isBigEndian
+                    )
+                }
+
+                AudioSystem.getClip().use { clip ->
+                    clip.open(audioFormat, pcmBytes, 0, pcmBytes.size)
+                    CompletableDeferred<Unit>().apply {
+                        clip.addLineListener { event ->
+                            if (event.type == LineEvent.Type.STOP) {
+                                complete(Unit)
+                            }
+                        }
+                        clip.start()
+                        await()
+                    }
+                }
+            }
+        }.onFailure { println("Playback error: ${it.message}") }
+    }
+
+    override fun close() = runBlocking {
+        mutex.withLock {
+            baos.close()
+        }
+    }
+}
--- example/src/main/kotlin/playback/PlaybackService.kt
+++ example/src/main/kotlin/playback/PlaybackService.kt
@@ -2,7 +2,7 @@ package playback
 
 import device.Device
 
-interface PlaybackService {
-    suspend fun play(device: Device, pcmBytes: ByteArray): Result<Unit>
-    suspend fun stop(): Result<Unit>
+interface PlaybackService : AutoCloseable {
+    suspend fun write(pcmBytes: ByteArray): Result<Unit>
+    suspend fun play(device: Device): Result<Unit>
 }
