d5ecf9d66c2b2be3a7fdf32545af22a31954cc75
numq <numq.dev@gmail.com>
2025-01-18 01:26:30 +0300

    Provide refactoring

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("com.microsoft.onnxruntime:onnxruntime:1.16.0-rc1")
+    implementation("com.microsoft.onnxruntime:onnxruntime:1.20.0")
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 }
